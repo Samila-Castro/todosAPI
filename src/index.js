fc33bdac31fd6cde722890f7de07cfaed3aacb15
Samila Castro <samilav1997@gmail.com>
2021-11-17 16:33:35 -0300

    Adiciona rota patch de todos

--- src/index.js
+++ src/index.js
@@ -79,6 +79,10 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 
   const todo = user.todos.find( todo => todo.id === id);
 
+  if(!todo) {
+    return response.status(404).json({ error: "Todo not found!"});
+  }
+
   todo.title = title;
   todo.deadline = new Date(deadline);
 
@@ -89,7 +93,19 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+    const { user } = request;
+    const { id } = request.params;
+  
+
+    const todo = user.todos.find( todo => todo.id === id);
+
+    if(!todo) {
+      return response.status(404).json({ error: "Todo not found!"});
+    }
+
+    todo.done = true;
+
+    return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
